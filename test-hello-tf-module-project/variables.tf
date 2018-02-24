758168bb521203665b542cf1ec435c7da2055a58
Rod Castillo <rod.castillo@me.com>
2018-02-24 13:43:25 -0500

    Add files via upload
    Added the files from the main Terraform project.

---
+++ test-hello-tf-module-project/variables.tf
@@ -0,0 +1,10 @@
+variable region {
+ default = "us-west-2"
+ description = "the aws region where we want create the resources"
+}
+
+
+variable myvpcid {
+default = "vpc-c2b758a5"
+ description = "you can choose the default vpc of the chosen region"
+}
